c80bb0d531eedbae336fb52c268ffbdca1fe8e48
Dragos Ciopeica <dragos.ciopeica@acrux.biz>
2020-10-05 18:06:36 +0300

    Dupa ce am schimbat Navbar si UserItem in functional classes

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
 
+// Folosim procesul numit: "Destructing". Extragem din map-ul care ne da "user", din Users.js ce avem nevoie.
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
     //Constructor este o metoda care mereu actioneaza cand componenta se incarca
     // constructor() {
     //     super(); // este obligatoriu sa chemam super, altfel va da eroare      
@@ -22,10 +24,15 @@ export class UserItem extends Component {
     //             html_url: "https://github.com/mojombo"
     //         }
 
-    render() {
+    
 
         // Folosim procesul numit: "Destructing". Extragem din this.state ce avem nevoie.com
-        const {login, avatar_url, html_url  } = this.props.user;
+        //DUPA ce am modificat UserItem din clasa in functie, "props" va fi transmis ca si parametru, sus in functie => Eliminam this.
+        
+                // Asta o sa o mutam sus, ca si parametru!
+        // const { login, avatar_url, html_url } = props.user;  
+        
+        
 
         return (
             <div className="card text-center">
@@ -37,6 +44,13 @@ export class UserItem extends Component {
             </div>
         )
     }
-}
 
-export default UserItem
+// Atentie la propTypes si PropTypes !!!!
+
+
+   UserItem.propTypes = {
+       user: PropTypes.object.isRequired
+   };
+
+
+export default UserItem;
